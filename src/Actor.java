3e27f49e205bde62f29faa68f0c19b3bd1d81ed1
GTashrif <156141491+GTashrif@users.noreply.github.com>
2025-09-20 22:44:32 +1000

    Task-6, 7, 8, 9 DONE

--- src/Actor.java
+++ src/Actor.java
@@ -5,9 +5,15 @@ public abstract class Actor {
   Color color;
   Cell loc;
 
+  public Actor(Cell inLoc, Color inColor) {
+    this.loc = inLoc;
+    this.color = inColor;
+  }
+
   public void paint(Graphics g) {
     g.setColor(color);
     g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+
     g.setColor(Color.GRAY);
     g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
   }
