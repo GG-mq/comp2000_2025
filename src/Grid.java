42286e86d9063cf6e255c5d24d23a648784d4b2a
Gazi Tashrif Gulzer Nadvi <gazitashrifgulzer.nadvi@students.mq.edu.au>
2025-09-21 19:19:12 +1000

    Update Grid.java

--- src/Grid.java
+++ src/Grid.java
@@ -17,7 +17,6 @@ public class Grid {
   }
   
   private CellType generateCellType() {
-    // Simple random cell type generation
     int type = random.nextInt(4);
     switch (type) {
       case 0: return new GrassCell();
