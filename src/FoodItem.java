905b2ce4191638458806642aa281974f2141c535
Gazi Tashrif Gulzer Nadvi <gazitashrifgulzer.nadvi@students.mq.edu.au>
2025-09-21 19:17:57 +1000

    added food item base class for logic

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,9 +1,5 @@
 import java.awt.Color;
 
-/**
- * Simple food item.
- * Demonstrates basic inheritance and interface implementation.
- */
 public class FoodItem extends Item implements Consumable {
     private String animalType;
     
