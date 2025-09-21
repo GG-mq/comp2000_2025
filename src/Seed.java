5aa29808bd8128418c552c11c9e8ef4dcea2bee5
Gazi Tashrif Gulzer Nadvi <gazitashrifgulzer.nadvi@students.mq.edu.au>
2025-09-21 19:22:22 +1000

    added seed food item for birds

--- src/Seed.java
+++ src/Seed.java
@@ -1,10 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-/**
- * Seed - can be eaten by birds.
- * Demonstrates basic inheritance.
- */
 public class Seed extends FoodItem {
     
     public Seed(int x, int y) {
@@ -13,12 +9,9 @@ public class Seed extends FoodItem {
     
     @Override
     protected void drawDetails(Graphics g) {
-        // Draw seed texture
         g.setColor(new Color(120, 80, 40));
         g.drawLine(x + 2, y + 2, x + size - 2, y + size - 2);
         g.drawLine(x + size - 2, y + 2, x + 2, y + size - 2);
-        
-        // Draw seed highlight
         g.setColor(new Color(200, 150, 100));
         g.fillOval(x + 3, y + 3, 2, 2);
     }
