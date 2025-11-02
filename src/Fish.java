63daad8804a842f9155d7feb091dd95284d69c9e
Gazi Tashrif Gulzer Nadvi <gazitashrifgulzer.nadvi@students.mq.edu.au>
2025-11-02 19:06:31 +1100

    create Fish food item for Cats
    Concrete food item that Cats can eat. Displays fish shape with tail, eye, 
    and stripes.
    
    Visual: Orange color (RGB: 255,180,50) with triangular tail
    Details: Tail fin, eye dot, vertical stripes
    Edible by: Cat only
    Score: +1 for player when consumed

---
+++ src/Fish.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+public class Fish extends FoodItem {
+    public Fish(int x, int y) {
+        super("Fish", new Color(255, 180, 50), x, y, "Cat");
+    }
+    @Override
+    protected void drawDetails(Graphics g) {
+        g.setColor(new Color(255, 200, 100));
+        int[] tailX = {x + size, x + size + 2, x + size};
+        int[] tailY = {y + 2, y + size/2, y + size - 2};
+        g.fillPolygon(tailX, tailY, 3);
+        g.setColor(Color.BLACK);
+        g.fillOval(x + 2, y + 2, 2, 2);
+        g.setColor(new Color(255, 150, 0));
+        g.drawLine(x + 3, y + 4, x + 3, y + size - 4);
+        g.drawLine(x + 5, y + 3, x + 5, y + size - 3);
+    }
+}
