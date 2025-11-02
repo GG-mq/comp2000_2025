1c2e6453d322863bf048e3ef6e010d4f75d1db07
Gazi Tashrif Gulzer Nadvi <gazitashrifgulzer.nadvi@students.mq.edu.au>
2025-11-02 18:53:58 +1100

    create Dog actor for autonomous AI
    Implements Dog actor with floppy ears and distinctive snout. Dog is computer-
    controlled and autonomously hunts for bones using pathfinding AI.
    
    Key features:
    - Rectangular face with floppy ear triangles
    - Facial details: eyes, nose, mouth line
    - Computer-controlled (autonomous AI)
    - Eats Bones only
    - Orange color scheme (RGB: 255,200,100)
    
    Visual design: Friendly dog appearance
    Integration: Autonomous opponent for competitive gameplay
    AI: Seeks nearest accessible bone every second

---
+++ src/Dog.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
+public class Dog extends Actor {
+    public Dog(Cell inLoc) {
+        super(inLoc, "Dog");
+        color = new Color(255, 200, 100);
+        display = new ArrayList<>();
+        updatePolygons();
+    }
+    @Override
+    protected void updatePolygons() {
+        display.clear();
+        Polygon ear1 = new Polygon();
+        ear1.addPoint(loc.x + 5, loc.y + 5);
+        ear1.addPoint(loc.x + 15, loc.y + 5);
+        ear1.addPoint(loc.x + 5, loc.y + 15);
+        Polygon ear2 = new Polygon();
+        ear2.addPoint(loc.x + 20, loc.y + 5);
+        ear2.addPoint(loc.x + 30, loc.y + 5);
+        ear2.addPoint(loc.x + 30, loc.y + 15);
+        Polygon face = new Polygon();
+        face.addPoint(loc.x + 8, loc.y + 7);
+        face.addPoint(loc.x + 27, loc.y + 7);
+        face.addPoint(loc.x + 27, loc.y + 25);
+        face.addPoint(loc.x + 8, loc.y + 25);
+        display.add(face);
+        display.add(ear1);
+        display.add(ear2);
+    }
+    @Override
+    protected void drawDetails(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillOval(loc.x + 12, loc.y + 12, 3, 3);
+        g.fillOval(loc.x + 20, loc.y + 12, 3, 3);
+        g.setColor(Color.BLACK);
+        g.fillOval(loc.x + 16, loc.y + 18, 2, 2);
+        g.drawLine(loc.x + 16, loc.y + 20, loc.x + 16, loc.y + 22);
+        g.drawLine(loc.x + 16, loc.y + 22, loc.x + 14, loc.y + 24);
+        g.drawLine(loc.x + 16, loc.y + 22, loc.x + 18, loc.y + 24);
+    }
+}
