4e8d847c6979e7750f6f243626e8bcc624916091
Gazi Tashrif Gulzer Nadvi <gazitashrifgulzer.nadvi@students.mq.edu.au>
2025-11-02 18:53:17 +1100

    create Cat actor with distinct visual design
    Implements Cat actor with triangular face, pointy ears, and whiskers. Cat can 
    eat Fish and is controlled by the player.
    
    Key features:
    - Triangle face polygon with two ear polygons
    - Facial details: eyes, nose, whiskers
    - Player-controlled (selectable)
    - Eats Fish only
    - Green color scheme (RGB: 100,150,255)
    
    Visual design: Recognizable cat silhouette with cartoon features
    Integration: Part of player's scoring team

---
+++ src/Cat.java
@@ -0,0 +1,44 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
+public class Cat extends Actor {
+    public Cat(Cell inLoc) {
+        super(inLoc, "Cat");
+        color = new Color(100, 150, 255);
+        display = new ArrayList<>();
+        updatePolygons();
+    }
+    @Override
+    protected void updatePolygons() {
+        display.clear();
+        Polygon ear1 = new Polygon();
+        ear1.addPoint(loc.x + 11, loc.y + 5);
+        ear1.addPoint(loc.x + 15, loc.y + 15);
+        ear1.addPoint(loc.x + 7, loc.y + 15);
+        Polygon ear2 = new Polygon();
+        ear2.addPoint(loc.x + 22, loc.y + 5);
+        ear2.addPoint(loc.x + 26, loc.y + 15);
+        ear2.addPoint(loc.x + 18, loc.y + 15);
+        Polygon face = new Polygon();
+        face.addPoint(loc.x + 5, loc.y + 15);
+        face.addPoint(loc.x + 29, loc.y + 15);
+        face.addPoint(loc.x + 17, loc.y + 30);
+        display.add(face);
+        display.add(ear1);
+        display.add(ear2);
+    }
+    @Override
+    protected void drawDetails(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillOval(loc.x + 12, loc.y + 18, 3, 3);
+        g.fillOval(loc.x + 20, loc.y + 18, 3, 3);
+        g.setColor(Color.PINK);
+        g.fillOval(loc.x + 16, loc.y + 22, 2, 2);
+        g.setColor(Color.BLACK);
+        g.drawLine(loc.x + 8, loc.y + 20, loc.x + 12, loc.y + 22);
+        g.drawLine(loc.x + 8, loc.y + 24, loc.x + 12, loc.y + 24);
+        g.drawLine(loc.x + 22, loc.y + 22, loc.x + 26, loc.y + 20);
+        g.drawLine(loc.x + 22, loc.y + 24, loc.x + 26, loc.y + 24);
+    }
+}
