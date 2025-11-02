c1b8f03d9ab1b2cea83f5ad9e35d43375abd4952
Gazi Tashrif Gulzer Nadvi <gazitashrifgulzer.nadvi@students.mq.edu.au>
2025-11-02 18:58:36 +1100

    create abstract CellType for terrain polymorphism
    Abstract base class for terrain types using Template Method pattern. Defines 
    common rendering structure with customizable pattern drawing.
    
    Key features:
    - Template Method: paint() calls abstract drawPattern()
    - Color and name fields for terrain identity
    - Highlight support for mouse interaction
    - Border rendering for grid visibility
    
    Design patterns: Template Method
    Benefits: Easy addition of new terrain types, consistent rendering structure
    Subclasses: GrassCell, WaterCell, SandCell, RockCell

---
+++ src/CellType.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+import java.awt.Graphics;
+public abstract class CellType {
+    protected Color color;
+    protected String name;
+    public CellType(Color color, String name) {
+        this.color = color;
+        this.name = name;
+    }
+    public void paint(Graphics g, int x, int y, int size, boolean isHighlighted) {
+        if (isHighlighted) {
+            g.setColor(new Color(200, 200, 200));
+        } else {
+            g.setColor(color);
+        }
+        g.fillRect(x, y, size, size);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, size, size);
+        drawPattern(g, x, y, size);
+    }
+    protected void drawPattern(Graphics g, int x, int y, int size) {}
+    public String getName() {
+        return name;
+    }
+}
